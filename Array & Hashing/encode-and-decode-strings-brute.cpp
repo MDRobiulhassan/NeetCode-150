54be829727b58810d88cd41a3a9081e56341b195
MDRobiulHassan <robiul110256@gmail.com>
2026-05-02 20:12:18 +0600

    Encode and Deocde String

--- Array & Hashing/encode-and-decode-strings-brute.cpp
+++ Array & Hashing/encode-and-decode-strings-brute.cpp
@@ -6,65 +6,30 @@ using namespace std;
 class Solution
 {
 public:
-    string encode(vector<string> &arr)
+    string encode(vector<string> &s)
     {
-        int n = arr.size();
-        string res = "";
-        for (int i = 0; i < n; i++)
-        {
-            res += to_string(arr[i].size()) + "/;" + arr[i];
-        }
-        return res;
+        string encodedString = "";
+
+        for (string str : s)
+            encodedString += (str + "#");
+
+        return encodedString;
     }
 
     vector<string> decode(string &s)
     {
-        vector<string> ans;
-        int i = 0, n = s.size();
-        while (i < n)
-        {
-            int j = i;
-            while (s[j] != '/')
-                j++;
-            int len = stoi(s.substr(i, j - i));
+        vector<string> decodedString;
 
-            i = j + 2;
-            ans.push_back(s.substr(i, len));
-            i += len;
+        int start = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '#')
+            {
+                decodedString.push_back(s.substr(start, i - start));
+                start = i + 1;
+            }
         }
 
-        return ans;
+        return decodedString;
     }
 };
-
-// Old Solution with # as a separator, which is not a good idea as the string can contain # as well
-// class Solution
-// {
-// public:
-//     string encode(vector<string> &s)
-//     {
-//         string encodedString = "";
-
-//         for (string str : s)
-//             encodedString += (str + "#");
-
-//         return encodedString;
-//     }
-
-//     vector<string> decode(string &s)
-//     {
-//         vector<string> decodedString;
-
-//         int start = 0;
-//         for (int i = 0; i < s.size(); i++)
-//         {
-//             if (s[i] == '#')
-//             {
-//                 decodedString.push_back(s.substr(start, i - start));
-//                 start = i + 1;
-//             }
-//         }
-
-//         return decodedString;
-//     }
-// };
--- Array & Hashing/encode-and-decode-strings-optimal.cpp
+++ Array & Hashing/encode-and-decode-strings-optimal.cpp
@@ -4,37 +4,72 @@ using namespace std;
 class Solution
 {
 public:
-    string encode(vector<string> &s)
+    string encode(vector<string> &arr)
     {
+        int n = arr.size();
         string res = "";
-
-        for (string &str : s)
-            res += (to_string(str.size()) + "#" + str);
-
+        for (int i = 0; i < n; i++)
+        {
+            res += to_string(arr[i].size()) + "/;" + arr[i];
+        }
         return res;
     }
 
     vector<string> decode(string &s)
     {
-
-        vector<string> res;
-
-        int i = 0;
-        while (i < s.size())
+        vector<string> ans;
+        int i = 0, n = s.size();
+        while (i < n)
         {
             int j = i;
-
-            while (s[j] != '#')
+            while (s[j] != '/')
                 j++;
-
             int len = stoi(s.substr(i, j - i));
-            string word = s.substr(j + 1, len);
 
-            res.push_back(word);
-
-            i = j + 1 + len;
+            i = j + 2;
+            ans.push_back(s.substr(i, len));
+            i += len;
         }
 
-        return res;
+        return ans;
     }
 };
+
+// Old Solution with # as a separator, which is not a good idea as the string can contain # as well
+// class Solution
+// {
+// public:
+//     string encode(vector<string> &s)
+//     {
+//         string res = "";
+
+//         for (string &str : s)
+//             res += (to_string(str.size()) + "#" + str);
+
+//         return res;
+//     }
+
+//     vector<string> decode(string &s)
+//     {
+
+//         vector<string> res;
+
+//         int i = 0;
+//         while (i < s.size())
+//         {
+//             int j = i;
+
+//             while (s[j] != '#')
+//                 j++;
+
+//             int len = stoi(s.substr(i, j - i));
+//             string word = s.substr(j + 1, len);
+
+//             res.push_back(word);
+
+//             i = j + 1 + len;
+//         }
+
+//         return res;
+//     }
+// };
