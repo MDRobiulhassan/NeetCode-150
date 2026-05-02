6f1616df1042a615196ad7afde9db8e73383ba17
MDRobiulHassan <robiul110256@gmail.com>
2026-05-02 20:11:35 +0600

    Encode and Deocde String

--- Array & Hashing/encode-and-decode-strings-brute.cpp
+++ Array & Hashing/encode-and-decode-strings-brute.cpp
@@ -6,30 +6,65 @@ using namespace std;
 class Solution
 {
 public:
-    string encode(vector<string> &s)
+    string encode(vector<string> &arr)
     {
-        string encodedString = "";
-
-        for (string str : s)
-            encodedString += (str + "#");
-
-        return encodedString;
+        int n = arr.size();
+        string res = "";
+        for (int i = 0; i < n; i++)
+        {
+            res += to_string(arr[i].size()) + "/;" + arr[i];
+        }
+        return res;
     }
 
     vector<string> decode(string &s)
     {
-        vector<string> decodedString;
-
-        int start = 0;
-        for (int i = 0; i < s.size(); i++)
+        vector<string> ans;
+        int i = 0, n = s.size();
+        while (i < n)
         {
-            if (s[i] == '#')
-            {
-                decodedString.push_back(s.substr(start, i - start));
-                start = i + 1;
-            }
+            int j = i;
+            while (s[j] != '/')
+                j++;
+            int len = stoi(s.substr(i, j - i));
+
+            i = j + 2;
+            ans.push_back(s.substr(i, len));
+            i += len;
         }
 
-        return decodedString;
+        return ans;
     }
 };
+
+// Old Solution with # as a separator, which is not a good idea as the string can contain # as well
+// class Solution
+// {
+// public:
+//     string encode(vector<string> &s)
+//     {
+//         string encodedString = "";
+
+//         for (string str : s)
+//             encodedString += (str + "#");
+
+//         return encodedString;
+//     }
+
+//     vector<string> decode(string &s)
+//     {
+//         vector<string> decodedString;
+
+//         int start = 0;
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             if (s[i] == '#')
+//             {
+//                 decodedString.push_back(s.substr(start, i - start));
+//                 start = i + 1;
+//             }
+//         }
+
+//         return decodedString;
+//     }
+// };
