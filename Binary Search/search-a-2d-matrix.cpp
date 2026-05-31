0dc52d838fe74273b9734a022209274016ee96d8
MDRobiulHassan <robiul110256@gmail.com>
2026-05-31 18:19:55 +0600

    LeetCodde 74 - Search a 2D Matrix

---
+++ Binary Search/search-a-2d-matrix.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int n = matrix.size(), m = matrix[0].size();
+        int l = 0, r = n * m - 1;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            int row = mid / m;
+            int col = mid % m;
+
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        return false;
+    }
+};
