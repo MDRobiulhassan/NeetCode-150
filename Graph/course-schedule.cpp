d99509f72112f254659785fd5676b10c2c23b769
MDRobiulHassan <robiul110256@gmail.com>
2026-08-15 21:13:30 +0600

    LeetCode 207. Course Schedule

---
+++ Graph/course-schedule.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites)
+        {
+            int u = it[0];
+            int v = it[1];
+            adj[u].push_back(v);
+        }
+
+        vector<int> indegree(numCourses);
+        for (int i = 0; i < numCourses; i++)
+        {
+            for (auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            count++;
+
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        return (count == numCourses);
+    }
+};
